1bab704d49af753723633f29c7d97ac4318e3a0b
phuwadech <aofphuwadech@gmail.com>
2025-01-17 14:26:41 +0700

    redirect https only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
     }
 
     /**
