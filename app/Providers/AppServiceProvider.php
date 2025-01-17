0a70dcbc0b2db94f4d626c529287c597a8ce9013
phuwadech <aofphuwadech@gmail.com>
2025-01-17 14:32:27 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
