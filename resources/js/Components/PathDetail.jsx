1204a683225e143a1b35197ea2eea7cc8b8316da
phuwadech <aofphuwadech@gmail.com>
2025-01-31 23:23:26 +0700

    fix path

--- resources/js/Components/PathDetail.jsx
+++ resources/js/Components/PathDetail.jsx
@@ -3,7 +3,7 @@ import {Box, Divider, Stack, Typography} from "@mui/material";
 export const PathDetail = () => (
     <div style={{marginTop : '1.5rem'}}>
         <Typography color='primary' fontWeight='bold'>
-            Home > แจ้งซ่อม > CK19016151
+            Home {'>'} แจ้งซ่อม {'>'} CK19016151
         </Typography>
         <Box width='100%' bgcolor='#f25822' sx={{height : 10,borderRadius : 1}}>
             <span style={{visibility : 'hidden'}}>
