0a5b7e1fa4f9e672e2b9f59609ab44465a2b73c5
phuwadech <aofphuwadech@gmail.com>
2025-03-03 09:09:00 +0700

    test image

--- resources/js/Pages/ReportRepair/Sp/SelectSP.jsx
+++ resources/js/Pages/ReportRepair/Sp/SelectSP.jsx
@@ -1,10 +1,29 @@
 import {Table, TableBody, TableCell, TableHead, TableRow, Typography} from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
 import {ImagePreview} from "@/Components/ImagePreview.jsx";
+import ProductImage from "@/Pages/ReportRepair/Sp/ProductImage.jsx";
+import {useEffect} from "react";
 
 
 export default function SelectSP({pid, list, selected, setSelected, warranty = false, sp_warranty = []}) {
     const spPath = import.meta.env.VITE_IMAGE_PATH + pid;
+
+    useEffect(() => {
+        console.log('hello')
+        Fetch();
+    }, []);
+
+    const Fetch = async () => {
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        fetch("https://images.pumpkin.tools/A%20%e0%b9%80%e0%b8%84%e0%b8%a3%e0%b8%b7%e0%b9%88%e0%b8%ad%e0%b8%87%e0%b8%a1%e0%b8%b7%e0%b8%ad%e0%b9%84%e0%b8%9f%e0%b8%9f%e0%b9%89%e0%b8%b2%20%e0%b9%81%e0%b8%a5%e0%b8%b0%20%e0%b9%80%e0%b8%84%e0%b8%a3%e0%b8%b7%e0%b9%88%e0%b8%ad%e0%b8%87%e0%b8%a1%e0%b8%b7%e0%b8%ad%e0%b9%84%e0%b8%9f%e0%b8%9f%e0%b9%89%e0%b8%b2%e0%b9%84%e0%b8%a3%e0%b9%89%e0%b8%aa%e0%b8%b2%e0%b8%a2/50227/SP50227-30.jpg", requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
+    }
 
     const handleOnChange = (item, e) => {
         const checked = e.target.checked;
@@ -30,6 +49,25 @@ export default function SelectSP({pid, list, selected, setSelected, warranty = f
                 }
         );
     };
+
+    const fetchImage = async (spcode) => {
+        const path = spPath + '/' + spcode + '.jpg'
+        try {
+            const {status} = await axios.get(path,{
+                headers : {
+                    "Content-Type" : 'image/jpeg'
+                }
+            });
+            console.log(status)
+            if (status === 200) {
+                return path;
+            }else{
+                return 'https://images.dcpumpkin.com/images/product/500/default.jpg';
+            }
+        }catch (error) {
+            console.log(error.response.status)
+        }
+    }
 
     return (
         <Table stickyHeader>
@@ -64,7 +102,10 @@ export default function SelectSP({pid, list, selected, setSelected, warranty = f
                             )}
                         </TableCell>
                         <TableCell>
-                            <ImagePreview src={spPath + '/' + item.spcode + '.jpg'}/>
+
+                            {/*<ImagePreview src={spPath + '/' + item.spcode + '.jpg'}/>*/}
+                            {/*<ProductImage spcode={item.spcode} spPath={spPath} />*/}
+
                         </TableCell>
                         <TableCell>{item.spcode}</TableCell>
                         <TableCell>{item.spname}</TableCell>
