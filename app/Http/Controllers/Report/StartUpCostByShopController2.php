ae3fc2d3cae68c507946d3f7ab53f200408dc610
phuwadech <aofphuwadech@gmail.com>
2026-06-25 15:21:38 +0700

    Roll Back stuc

--- app/Http/Controllers/Report/StartUpCostByShopController2.php
+++ app/Http/Controllers/Report/StartUpCostByShopController2.php
@@ -57,6 +57,7 @@ class StartUpCostByShopController2 extends Controller
             ->select('job_lists.*', 'store_information.shop_name')
             ->where('job_lists.status', 'success')
             ->where('job_lists.warranty', true)
+            ->where('job_lists.stuc_status', 'Y')
             ->whereExists(function ($sub) {
                 $sub->select(DB::raw(1))
                     ->from('spare_parts')
@@ -155,6 +156,7 @@ class StartUpCostByShopController2 extends Controller
         $query = JobList::query()
             ->where('status', 'success')
             ->where('warranty', true)
+            ->where('job_lists.stuc_status', 'Y')
             ->whereExists(function ($sub) {
                 $sub->select(DB::raw(1))
                     ->from('spare_parts')
